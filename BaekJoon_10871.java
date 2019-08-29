4e6f4f818fa96cc844806e5a51515596d7441bcc
hyunseok4475 <33482120+hyunseok4475@users.noreply.github.com>
2019-08-29 14:08:58 +0900

    Solve For

--- BaekJoon_10871.java
+++ BaekJoon_10871.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 
-public class BaekJoon_2439 {
+public class BaekJoon_10871 {
     public static void main(String[] args){
         Scanner stdIn = new Scanner(System.in);
 
         int a = stdIn.nextInt();
+        int b = stdIn.nextInt();
+        int[] list = new int[a];
 
         for(int i = 0; i < a; i++){
-            for(int j = a - (i+1); j > 0; j--){
+            list[i] = stdIn.nextInt();
+        }
+
+        for(int i =0; i < a; i++){
+            if(list[i] < b){
+                System.out.print(list[i]);
                 System.out.print(" ");
             }
-            for(int k = 0; k <= i; k++){
-                System.out.print("*");
-            }
-            System.out.print("\n");
         }
     }
 }
